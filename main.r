faf88ef23c4fe37d8e3461560aa7aba9a80de4e0
Francesco Prem Solidoro <franci.solidoro@gmail.com>
2025-03-18 00:43:48 +0100

    main: remove useless column
    there's a mostly empty column "Important notes for fiscal data (mouse
    over to see comment)" that I removed from the quantitative analysis

--- main.r
+++ main.r
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(readxl)
 
-df <- read_excel("./data/Freedom_In_The_50_States_2023.xlsx")
+df <- read_excel("./data/Freedom_In_The_50_States_2023.xlsx") %>%
+        select(-"Important notes for fiscal data (mouse over to see comment)")
 glimpse(df)
