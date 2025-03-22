2c1ce66b99af9ca6dd489d6182de6b4dd7a16bf1
Francesco Prem Solidoro <franci.solidoro@gmail.com>
2025-03-22 15:18:14 +0100

    main.r: delete

--- main.r
+++
@@ -1,7 +0,0 @@
-library(tidyverse)
-library(paletteer) #best library in CRAN https://pmassicotte.github.io/paletteer_gallery/
-library(readxl)
-
-load(file="./data/gov_transfers.rda")
-View(gov_transfers)
-
