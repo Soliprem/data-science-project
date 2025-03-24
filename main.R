c0adff893b9aa0d06983c219817181618e4dae59
Francesco Prem Solidoro <franci.solidoro@gmail.com>
2025-03-24 16:54:10 +0100

    main: linter pass

--- main.R
+++ main.R
@@ -1,7 +1,7 @@
-#The effects of participation in cash transfer programs on political support for the government
-#Aydin, Salvi and Solidoro
+# The effects of participation in cash transfer programs on political support for the government
+# Aydin, Salvi and Solidoro
 
-#Loading the required libraries
+# Loading the required libraries
 library(tidyverse)
 library(kableExtra)
 library(GGally)
@@ -9,13 +9,13 @@ library(ggpubr)
 library(rddensity)
 library(modelsummary)
 
-#Importing and editing the data
+# Importing and editing the data
 load("data/gov_transfers.rda")
 
-gov_transfers <- gov_transfers |> 
+gov_transfers <- gov_transfers |>
   rename(Normalized_Income = Income_Centered)
 
-#Exploring the data
+# Exploring the data
 head(gov_transfers)
 
 # Exploring the data
@@ -24,7 +24,7 @@ gov_transfers |>
   kable("markdown") |>
   save_kable("artifacts/gov_transfers.md")
 
-gov_transfers |> 
+gov_transfers |>
   ggpairs()
 
 # Checking for fuzziness
@@ -34,99 +34,102 @@ gov_transfers |>
   kable("markdown") |>
   save_kable("artifacts/gov_transfers_fuzziness.md")
 
-gov_transfers |> 
+gov_transfers |>
   ggplot(aes(x = Normalized_Income, y = Participation)) +
   geom_vline(xintercept = 0, color = "gray") +
   geom_point(color = "darkcyan", position = position_jitter(width = 0, height = 0.12)) +
   theme_light()
 
-#Checking for manipulation
-density <- gov_transfers$Normalized_Income |> 
+# Checking for manipulation
+density <- gov_transfers$Normalized_Income |>
   rddensity(c = 0)
 
-density |> 
-  summary() |> 
-  capture.output() |> 
+density |>
+  summary() |>
+  capture.output() |>
   kable()
 
 density |>
   rdplotdensity(X = gov_transfers$Normalized_Income, type = "both", xlabel = "Normalized Income", ylabel = "Density")
 
-#Creating tibbles by different bandwiths
-quart_bw <- gov_transfers |> 
+# Creating tibbles by different bandwiths
+quart_bw <- gov_transfers |>
   filter(Normalized_Income > -0.005 & Normalized_Income < 0.005)
 
-half_bw <- gov_transfers |> 
+half_bw <- gov_transfers |>
   filter(-0.01 < Normalized_Income & Normalized_Income < 0.01)
 
-#RDD graphing by different specifications
-full_graph <- gov_transfers |> 
+# RDD graphing by different specifications
+full_graph <- gov_transfers |>
   ggplot(aes(x = Normalized_Income, y = Support, group = Participation)) +
-  geom_point(color = "darkcyan") + 
+  geom_point(color = "darkcyan") +
   geom_vline(xintercept = 0, color = "gray") +
-  geom_smooth(method = "lm", color = "navy", fill = "lightblue") + 
+  geom_smooth(method = "lm", color = "navy", fill = "lightblue") +
   theme_light()
 
-full_graph_q <- gov_transfers |> 
+full_graph_q <- gov_transfers |>
   ggplot(aes(x = Normalized_Income, y = Support, group = Participation)) +
-  geom_point(color = "darkcyan") + 
+  geom_point(color = "darkcyan") +
   geom_vline(xintercept = 0, color = "gray") +
-  geom_smooth(method = "lm", formula = y ~ poly(x, degree = 2), color = "navy", fill = "lightblue") + 
+  geom_smooth(method = "lm", formula = y ~ poly(x, degree = 2), color = "navy", fill = "lightblue") +
   theme_light()
 
-full_graph_c <- gov_transfers |> 
+full_graph_c <- gov_transfers |>
   ggplot(aes(x = Normalized_Income, y = Support, group = Participation)) +
-  geom_point(color = "darkcyan") + 
+  geom_point(color = "darkcyan") +
   geom_vline(xintercept = 0, color = "gray") +
-  geom_smooth(method = "lm", formula = y ~ poly(x, degree = 3), color = "navy", fill = "lightblue") + 
+  geom_smooth(method = "lm", formula = y ~ poly(x, degree = 3), color = "navy", fill = "lightblue") +
   theme_light()
 
-half_graph <- half_bw |> 
+half_graph <- half_bw |>
   ggplot(aes(x = Normalized_Income, y = Support, group = Participation)) +
-  geom_point(color = "darkcyan") + 
+  geom_point(color = "darkcyan") +
   geom_vline(xintercept = 0, color = "gray") +
-  geom_smooth(method = "lm", color = "navy", fill = "lightblue") + 
+  geom_smooth(method = "lm", color = "navy", fill = "lightblue") +
   theme_light()
 
-half_graph_q <- half_bw |> 
+half_graph_q <- half_bw |>
   ggplot(aes(x = Normalized_Income, y = Support, group = Participation)) +
-  geom_point(color = "darkcyan") + 
+  geom_point(color = "darkcyan") +
   geom_vline(xintercept = 0, color = "gray") +
-  geom_smooth(method = "lm", formula = y ~ poly(x, degree = 2), color = "navy", fill = "lightblue") + 
+  geom_smooth(method = "lm", formula = y ~ poly(x, degree = 2), color = "navy", fill = "lightblue") +
   theme_light()
 
-half_graph_c <- half_bw |> 
+half_graph_c <- half_bw |>
   ggplot(aes(x = Normalized_Income, y = Support, group = Participation)) +
-  geom_point(color = "darkcyan") + 
+  geom_point(color = "darkcyan") +
   geom_vline(xintercept = 0, color = "gray") +
-  geom_smooth(method = "lm", formula = y ~ poly(x, degree = 3), color = "navy", fill = "lightblue") + 
+  geom_smooth(method = "lm", formula = y ~ poly(x, degree = 3), color = "navy", fill = "lightblue") +
   theme_light()
 
-quart_graph <- quart_bw |> 
+quart_graph <- quart_bw |>
   ggplot(aes(x = Normalized_Income, y = Support, group = Participation)) +
-  geom_point(color = "darkcyan") + 
+  geom_point(color = "darkcyan") +
   geom_vline(xintercept = 0, color = "gray") +
-  geom_smooth(method = "lm", color = "navy", fill = "lightblue") + 
+  geom_smooth(method = "lm", color = "navy", fill = "lightblue") +
   theme_light()
 
-quart_graph_q <- quart_bw |> 
+quart_graph_q <- quart_bw |>
   ggplot(aes(x = Normalized_Income, y = Support, group = Participation)) +
-  geom_point(color = "darkcyan") + 
+  geom_point(color = "darkcyan") +
   geom_vline(xintercept = 0, color = "gray") +
-  geom_smooth(method = "lm", formula = y ~ poly(x, degree = 2), color = "navy", fill = "lightblue") + 
+  geom_smooth(method = "lm", formula = y ~ poly(x, degree = 2), color = "navy", fill = "lightblue") +
   theme_light()
 
-quart_graph_c <- quart_bw |> 
+quart_graph_c <- quart_bw |>
   ggplot(aes(x = Normalized_Income, y = Support, group = Participation)) +
-  geom_point(color = "darkcyan") + 
+  geom_point(color = "darkcyan") +
   geom_vline(xintercept = 0, color = "gray") +
-  geom_smooth(method = "lm", formula = y ~ poly(x, degree = 3), color = "navy", fill = "lightblue") + 
+  geom_smooth(method = "lm", formula = y ~ poly(x, degree = 3), color = "navy", fill = "lightblue") +
   labs(xlab = "", ylab = "") +
   theme_light()
 
-ggarrange(plotlist = list(full_graph, full_graph_q, full_graph_c, half_graph, half_graph_q, half_graph_c, quart_graph, quart_graph_q, quart_graph_c), ncol = 3, nrow = 3)
+ggarrange(plotlist = list(
+  full_graph, full_graph_q, full_graph_c,
+  half_graph, half_graph_q, half_graph_c, quart_graph, quart_graph_q, quart_graph_c
+), ncol = 3, nrow = 3)
 
-#RDD calculation by different specifications
+# RDD calculation by different specifications
 full_lm <- lm(Support ~ Participation + Normalized_Income, data = gov_transfers)
 
 half_lm <- lm(Support ~ Participation + Normalized_Income, data = half_bw)
@@ -139,7 +142,7 @@ half_q <- lm(Support ~ Participation + poly(Normalized_Income, degree = 2), data
 
 quart_q <- lm(Support ~ Participation + poly(Normalized_Income, degree = 2), data = quart_bw)
 
-full_c<- lm(Support ~ Participation + poly(Normalized_Income, degree = 3), data = gov_transfers)
+full_c <- lm(Support ~ Participation + poly(Normalized_Income, degree = 3), data = gov_transfers)
 
 half_c <- lm(Support ~ Participation + poly(Normalized_Income, degree = 3), data = half_bw)
 
@@ -151,20 +154,28 @@ half_slope <- lm(Support ~ Participation + Normalized_Income + Participation * N
 
 quart_slope <- lm(Support ~ Participation + Normalized_Income + Participation * Normalized_Income, data = quart_bw)
 
-full_age <- lm(Support ~ Participation + Normalized_Income + Participation * Normalized_Income + Age, data = gov_transfers)
+full_age <- lm(Support ~ Participation + Normalized_Income +
+  Participation * Normalized_Income + Age, data = gov_transfers)
 
-half_age <- lm(Support ~ Participation + Normalized_Income + Participation * Normalized_Income + Age, data = gov_transfers)
+half_age <- lm(Support ~ Participation + Normalized_Income +
+  Participation * Normalized_Income + Age, data = gov_transfers)
 
 quart_age <- lm(Support ~ Participation + Normalized_Income + Participation * Normalized_Income + Age, data = half_bw)
 
 full_specs <- msummary(
-  list(full_lm, full_q, full_c, full_slope, full_age), output = "markdown", stars = T)
+  list(full_lm, full_q, full_c, full_slope, full_age),
+  output = "markdown", stars = T
+)
 full_specs
 
 half_specs <- msummary(
-  list(half_lm, half_q, half_c, half_slope, half_age), output = "markdown", stars = T)
+  list(half_lm, half_q, half_c, half_slope, half_age),
+  output = "markdown", stars = T
+)
 half_specs
 
 quart_specs <- msummary(
-  list(quart_lm, quart_q, quart_c, quart_slope, quart_age), output = "markdown", stars = T)
+  list(quart_lm, quart_q, quart_c, quart_slope, quart_age),
+  output = "markdown", stars = T
+)
 quart_specs
